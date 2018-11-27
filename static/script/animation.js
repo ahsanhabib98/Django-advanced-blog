ec001c789e2209e8aac5a996979485a0c2f053ce
ahsanhabib98 <ahredoan@gmail.com>
2018-11-28 01:33:58 +0600

    updated blog

---
+++ static/script/animation.js
@@ -0,0 +1,57 @@
+
+window.sr = ScrollReveal();
+
+sr.reveal('.title', {
+    duration: 2000,
+    origin: 'left',
+    distance: '500px'
+});
+sr.reveal('.search', {
+    duration: 2000,
+    delay: 1500,
+    origin: 'bottom'
+});
+sr.reveal('.article', {
+    duration: 2000,
+    origin: 'left',
+    distance: '200px'
+});
+sr.reveal('.box', {
+    duration: 2000,
+    origin: 'right',
+    distance: '200px'
+});
+sr.reveal('.powered-by', {
+    duration: 2000,
+    delay: 500,
+    origin: 'top',
+    distance: '150px'
+});
+sr.reveal('.newsLetter', {
+    duration: 2000,
+    delay: 500,
+    origin: 'left',
+    distance: '150px'
+});
+sr.reveal('.contact', {
+    duration: 2000,
+    delay: 500,
+    origin: 'right',
+    distance: '150px'
+});
+
+sr.reveal('.postDetails', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px'
+});
+sr.reveal('.about', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+sr.reveal('.social-link', {
+    duration: 2000,
+    origin: 'right',
+    distance: '150px'
+});
